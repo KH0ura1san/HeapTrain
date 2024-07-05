bc0e9ebdb0afd53ebf5ff8d41d06466d2aaa3092
KH0ura1san <Gwh233@outlook.com>
2024-07-05 11:24:48 +0800

    Create Project

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include "heap.c"
 #include "leak.c"
+#include "utils.c"
 
 int main(void) {
-    printf("Hello, World!\n");
-
-    return 0;
+    logo();
+    while(1){
+        menu();
+        int c = read_num();
+        switch (c) {
+            case 1:
+                add_chunk();
+                break;
+            case 2:
+                delete_chunk();
+                break;
+            case 3:
+                edit();
+                break;
+            case 4:
+                show();
+                break;
+            case 5:
+                exit(0);
+            default:
+                puts("Invalid choice.");
+        }
+    }
 }
